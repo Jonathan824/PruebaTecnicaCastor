31cf74e2d183ddad728beda1b6a0dda45f3e87ea
jonathan824 <jonathan.m.j.824@gmail.com>
2022-11-03 17:38:42 -0500

    Se agrega funciones de elminiar registro

--- pruebaTecnicaFrontend/src/app/COMPONENTES/inicio/inicio.component.ts
+++ pruebaTecnicaFrontend/src/app/COMPONENTES/inicio/inicio.component.ts
@@ -8,21 +8,27 @@ import { EmpleadoService } from 'src/app/SERVICE/empleado.service';
 })
 export class InicioComponent implements OnInit {
 
-  lista:any=[];
+  lista: any = [];
   constructor(private empleadoService: EmpleadoService) { }
 
   ngOnInit(): void {
     this.listarEmpleados();
-  } 
+  }
 
-  listarEmpleados()
-  {
+  listarEmpleados() {
     this.empleadoService.getEmpleados().subscribe(
-      res=>{this.lista=res;
-            console.log(res);
-          },
-      err=>console.log(err)
+      res => {
+        this.lista = res;
+        console.log(res);
+      },
+      err => console.log(err)
     );
   }
 
+  eliminar(id: string) {
+    this.empleadoService.deleteEmpleado(id).subscribe(
+      res => { this.ngOnInit(); },
+      err => console.log(err)
+    );
+  }
 }
