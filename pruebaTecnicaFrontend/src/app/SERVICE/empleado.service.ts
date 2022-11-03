8849be74467bc6dcbaac23588e08986140501cfe
jonathan824 <jonathan.m.j.824@gmail.com>
2022-11-03 02:02:20 -0500

    Se usa http de angular y se le pasa por el método get la rutaa

--- pruebaTecnicaFrontend/src/app/SERVICE/empleado.service.ts
+++ pruebaTecnicaFrontend/src/app/SERVICE/empleado.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadoService {
 
-  constructor() { }
+  url='http://localhost:8080/api/empleados';
+  constructor(private http: HttpClient) { }
+
+  getEmpleados():Observable<any>
+  {
+    return this.http.get(this.url);
+  }
 }
