cb8593d16e3233e3248ecbb3377308906ba24aab
jonathan824 <jonathan.m.j.824@gmail.com>
2022-11-03 17:32:26 -0500

    Se agrega funcionalidades del agregar

--- pruebaTecnicaFrontend/src/app/COMPONENTES/agregar/agregar.component.ts
+++ pruebaTecnicaFrontend/src/app/COMPONENTES/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Empleado, EmpleadoService} from 'src/app/SERVICE/empleado.service'
 
 @Component({
@@ -9,9 +10,19 @@ import { Empleado, EmpleadoService} from 'src/app/SERVICE/empleado.service'
 export class AgregarComponent implements OnInit {
 
   empleadoNuevo: Empleado={id:'',cedula:'',nombre:'',fecha:'',ruta:'',cargo:''};
-  constructor(private empleadoService: EmpleadoService) { }
+   
+  constructor(private empleadoService: EmpleadoService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  agregarEmpleado(){
+    this.empleadoService.saveEmpleado(this.empleadoNuevo).subscribe(
+      res=>{
+        console.log(res);
+        this.router.navigate(['/inicio']);
+      },
+      err=>console.log(err)
+    );
+  }
 }
